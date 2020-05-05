130db1138762ca780f60984a70955cf0e199a738
Alexander Kolov <me@alexkolov.com>
2020-05-05 14:12:06 +0900

    WeakMap

---
+++ Sources/RxExtensions/MapWeak.swift
@@ -0,0 +1,19 @@
+//
+//  MapWeak.swift
+//  RxExtensions
+//
+//  Created by Alexander Kolov on 2020-05-05.
+//  Copyright © 2020 Alexander Kolov. All rights reserved.
+//
+
+import RxSwift
+
+extension ObservableType {
+
+  public func map<A: AnyObject, ResultType>(weak obj: A, selector: @escaping (A, Self.Element) throws -> ResultType) -> Observable<ResultType?> {
+    return map { [weak obj] value -> ResultType? in
+      try obj.map { try selector($0, value) }
+    }
+  }
+
+}
